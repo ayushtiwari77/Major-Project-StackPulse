0444b1abf3b5efd1f04921e81e2f3d2121da04ec
Ayush Tiwari <codewithayush77@gmail.com>
2025-06-25 01:55:57 +0530

    Update redis.service.js

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,17 +1,17 @@
-import Redis from "ioredis";
+//import Redis from "ioredis";
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-});
+//const redisClient = new Redis({
+//  host: process.env.REDIS_HOST,
+//  port: process.env.REDIS_PORT,
+ // password: process.env.REDIS_PASSWORD,
+//});
 
-redisClient.on("connect", () => {
-  console.log("Redis Connected");
-});
+//redisClient.on("connect", () => {
+//  console.log("Redis Connected");
+//});
 
-redisClient.on("error", (err) => {
-  console.error("Redis Connection Error:", err);
-});
+//redisClient.on("error", (err) => {
+//  console.error("Redis Connection Error:", err);
+//});
 
-export default redisClient;
+//export default redisClient;
